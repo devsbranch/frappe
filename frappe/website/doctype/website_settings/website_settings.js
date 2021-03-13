2ca83e673b8a10db8c9e98f18b61eacc395a6247
Alison Mukoma <alison.mukoma@trivago.com>
2021-03-13 19:36:07 +0100

    update link title

--- frappe/website/doctype/website_settings/website_settings.js
+++ frappe/website/doctype/website_settings/website_settings.js
@@ -13,8 +13,8 @@ frappe.ui.form.on('Website Settings', {
 	},
 
 	refresh: function(frm) {
-		frm.add_custom_button(__('View Website'), () => {
-			window.open('/', '_blank');
+		frm.add_custom_button(__('Home'), () => {
+			window.open('/app', '_blank');
 		});
 	},
 
